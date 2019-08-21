6336cbe3a8fd477265838b9a9c25d7f267984289
virajransing <virajransing@gmail.com>
2019-08-21 17:27:28 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,15 +3,15 @@ sum = num1 + num 2
 end
 
 def subtraction(num1, num2)
-difference num1 - num2
+difference = num1 - num2
 end
 
 def division(num1, num2)
-quotuient num1 / num2
+quotuient = num1 / num2
 end
 
 def multiplication(num1, num2)
-product num1 * num2
+product = num1 * num2
 end
 
 def modulo(num1, num2)
@@ -19,5 +19,5 @@ remainder = num1 % num2
 end
 
 def square_root(num)
-square_rt = 
+square_rt = Math.sqrt(num)
 end
